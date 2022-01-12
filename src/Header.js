75f700def90c16c29bb48349b4566fdd2d1c0160
Gökberk Hıdıroğlu <gokberkhdrgl@gmail.com>
2022-01-12 17:14:05 +0300

    header and css created.

---
+++ src/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Header.css";
+import HeaderOption from "./HeaderOption";
+import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
+          alt=""
+        />
+
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" placeholder="Search" />
+        </div>
+      </div>
+
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption
+          avatar="https://avatars.githubusercontent.com/u/25930033?v=4"
+          title="me"
+        />
+      </div>
+    </div>
+  );
+}
