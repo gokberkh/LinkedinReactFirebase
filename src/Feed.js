405d9f40ac3a633f5694c47d4ec9a669c2f68cb0
busemzglu <gokbey5801@gmail.com>
2022-01-13 00:08:34 +0300

    feed component created.

---
+++ src/Feed.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Feed.css";
+import CreateIcon from "@material-ui/icons/Create";
+import InputOption from "./InputOption.js";
+import ImageIcon from "@material-ui/icons/Image";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
+export default function Feed() {
+  return (
+    <div className="feed">
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <CreateIcon />
+          <form>
+            <input type="text" />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+        <div className="feed__inputOptions">
+          <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="Write Article"
+            color="#7FC15E"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
