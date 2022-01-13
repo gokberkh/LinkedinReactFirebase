1cbdd5bdcee8fea4314f8119818febc38a2b416f
Gökberk Hıdıroğlu <gokberkhdrgl@gmail.com>
2022-01-13 22:09:46 +0300

    firebase auth and redux added.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Header from "./Header.js";
+import Sidebar from "./Sidebar.js";
+import Feed from "./Feed.js";
+import Login from "./Login.js";
+import Widgets from "./Widgets.js";
+import { useSelector, useDispatch } from "react-redux";
+import { selectUser, logout, login } from "./features/userSlice";
+import { auth } from "./firebaseConfig";
 import "./App.css";
-import Header from "./Header";
-import Sidebar from "./Sidebar";
-import Feed from "./Feed";
+
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+            photoUrl: userAuth.photoURL,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
+
   return (
-    <div className="app">
-      {/* Header */}
-      <Header />
-      {/* App Body */}
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        {/* Feedback */}
-        {/* Widgets */}
-      </div>
+    <div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app">
+          <Header />
+          <div className="app__body">
+            <Sidebar />
+            <Feed />
+            <Widgets />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
