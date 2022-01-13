f6f8a8ee3db2cd6c117988c86eef279e907ce28d
Gökberk Hıdıroğlu <gokberkhdrgl@gmail.com>
2022-01-13 22:10:16 +0300

    widgets created.

---
+++ src/Widgets.js
@@ -0,0 +1,41 @@
+import React from "react";
+import InfoIcon from "@material-ui/icons/Info";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+import "./Widgets.css";
+
+function Widgets() {
+  const newsArticle = (heading, subtitle) => (
+    <div className="widgets__article">
+      <div className="widgets__articleLeft">
+        <FiberManualRecordIcon />
+      </div>
+      <div className="widgets__articleRight">
+        <h4>{heading}</h4>
+        <p>{subtitle}</p>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="widgets">
+      <div className="widgets__header">
+        <h2>LinkedIn News</h2>
+        <InfoIcon />
+      </div>
+      {newsArticle(
+        "Coronavirus Cases",
+        "The death toll is on the rise - 10K readers"
+      )}
+      {newsArticle(
+        "The Empire Strikes Back",
+        "Star Wars movie cancelled by the people - 4K readers"
+      )}
+      {newsArticle(
+        "Merry Christmas",
+        "Everyone worldwide experiences their first lockdown christmas - 2K readers"
+      )}
+    </div>
+  );
+}
+
+export default Widgets;
