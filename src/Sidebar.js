34dfeb65cdcaf273ae98c4485225934fa6b83df6
Gökberk Hıdıroğlu <gokberkhdrgl@gmail.com>
2022-01-14 21:08:00 +0300

    sidebar redux user added.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./Sidebar.css";
 import { Avatar } from "@material-ui/core";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
 
-export default function Sidebar() {
+function Sidebar() {
+  const user = useSelector(selectUser);
   const recentItem = (topic) => (
     <div className="sidebar__recentItem">
       <span className="sidebar__hash">#</span>
@@ -14,34 +17,37 @@ export default function Sidebar() {
     <div className="sidebar">
       <div className="sidebar__top">
         <img
-          src="https://png.pngtree.com/thumb_back/fh260/background/20200714/pngtree-modern-double-color-futuristic-neon-background-image_351866.jpg"
-          alt=""
+          src="https://blog.biletbayi.com/wp-content/uploads/2020/03/sivas-turkiye-scaled.jpg"
+          alt="background-banner"
         />
+
         <Avatar
+          style={{ height: "60px", width: "60px" }}
+          src={user.photoUrl}
           className="sidebar__avatar"
-          src="https://avatars.githubusercontent.com/u/25930033?v=4"
         />
-        <h2>Gökberk Hıdıroğlu</h2>
-        <h4>gokberkhdrgl@gmail.com</h4>
+        <h2>{user.displayName}</h2>
+        <h4>{user.email}</h4>
       </div>
       <div className="sidebar__stats">
         <div className="sidebar__stat">
           <p>Who viewed you</p>
-          <p className="sidebar__statNumber">5,858</p>
+          <p className="sidebar__statNumber">58,000</p>
         </div>
         <div className="sidebar__stat">
           <p>Views on post</p>
-          <p className="sidebar__statNumber">5,858</p>
+          <p className="sidebar__statNumber">5800</p>
         </div>
       </div>
       <div className="sidebar__bottom">
         <p>Recent</p>
         {recentItem("reactjs")}
         {recentItem("programming")}
-        {recentItem("software")}
-        {recentItem("developer")}
-        {recentItem("design")}
+        {recentItem("software development")}
+        {recentItem("graphic design")}
       </div>
     </div>
   );
 }
+
+export default Sidebar;
