0639d57aeeb87cf567874148926965b4a5b58bc7
Gökberk Hıdıroğlu <gokberkhdrgl@gmail.com>
2022-01-15 23:44:35 +0300

    header notification added.

--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./HeaderOption.css";
-import { Avatar } from "@material-ui/core";
+import { Avatar, Badge } from "@material-ui/core";
 
-export default function HeaderOption({ avatar, Icon, title, onClick }) {
+export default function HeaderOption({
+  avatar,
+  Icon,
+  title,
+  onClick,
+  badgeContent,
+}) {
   return (
     <div onClick={onClick} className="headerOption">
-      {Icon && <Icon className="headerOption__icon" />}
+      {Icon && (
+        <Badge badgeContent={badgeContent} overlap="circular" color="secondary">
+          <Icon className="headerOption__icon"  />
+        </Badge>
+      )}
       {avatar && <Avatar className="headerOption__icon" src={avatar} />}
       <h3 className="headerOption__title">{title}</h3>
     </div>
